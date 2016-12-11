5d7355ad1808934990c94212b5b93786028fb23f
Jon Moss <me@jonathanmoss.me>
2016-12-11 11:04:08 -0500

    Use old fashioned tag code
    The new `tag.script` code is not available on `5-0-stable`; it is onl
    available on Rails master, which is tracking 5.1.
    
    Discovered this while trying to write a test for this helper method. :)

--- lib/webpacker/helper.rb
+++ lib/webpacker/helper.rb
@@ -2,6 +2,6 @@ require 'webpacker/source'
 
 module Webpacker::Helper
   def javascript_pack_tag(name, **options)
-    tag.script src: Webpacker::Source.new(name).path, **options
+    tag('script', src: Webpacker::Source.new(name).path, **options)
   end
 end
