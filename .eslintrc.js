9c1d98e275ffc554defa3d1911211af4d497c061
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-02-24 16:03:42 +0000

    Add rules to disable semicolon and trailing comma

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
   "extends": "airbnb",
   "rules": {
+    "comma-dangle": ["error", "never"],
     "import/no-unresolved": "off",
     "import/no-extraneous-dependencies": "off",
     "import/extensions": "off",
     "no-console": "off",
+    semi: ['error', 'never'],
   },
   "env": {
-    "node": true,
     "browser": true,
+    "node": true,
   },
 };
--- lib/install/react/hello_react.jsx
+++ lib/install/react/hello_react.jsx
@@ -2,24 +2,24 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 const Hello = props => (
   <div>Hello {props.name}!</div>
-);
+)
 
 Hello.defaultProps = {
-  name: 'David',
-};
+  name: 'David'
+}
 
 Hello.propTypes = {
-  name: React.PropTypes.string,
-};
+  name: React.PropTypes.string
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Hello name="React" />,
     document.body.appendChild(document.createElement('div')),
-  );
-});
+  )
+})
