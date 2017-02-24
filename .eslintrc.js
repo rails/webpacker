8f38dfb281839c12d872860963fdf97f07fca5eb
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-02-24 16:09:36 +0000

    Use single quote for strings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  "extends": "airbnb",
-  "rules": {
-    "comma-dangle": ["error", "never"],
-    "import/no-unresolved": "off",
-    "import/no-extraneous-dependencies": "off",
-    "import/extensions": "off",
-    "no-console": "off",
+  'extends': 'airbnb',
+  'rules': {
+    'comma-dangle': ['error', 'never'],
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': 'off',
+    'no-console': 'off',
     semi: ['error', 'never'],
   },
-  "env": {
-    "browser": true,
-    "node": true,
+  'env': {
+    'browser': true,
+    'node': true,
   },
 };
