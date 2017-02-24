2ad9d13ac20e18a31452e4e721677421c57a2670
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-02-24 14:47:12 +0000

    Fix typo

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,6 @@ module.exports = {
     "import/no-unresolved": "off",
     "import/no-extraneous-dependencies": "off",
     "import/extensions": "off",
-    "func-names": "off",
-    "prefer-arrow-callback": "off",
-    "object-shorthand": "off",
     "no-console": "off",
   },
   "globals": {
