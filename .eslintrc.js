d05106dd9c64b41eaaf6c89c90750f83555156bf
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-02-24 15:56:09 +0000

    Remove duplicate plugins and use env to target browser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,13 @@
 module.exports = {
   "extends": "airbnb",
-  "plugins": [
-    "react",
-    "jsx-a11y",
-    "import"
-  ],
   "rules": {
     "import/no-unresolved": "off",
     "import/no-extraneous-dependencies": "off",
     "import/extensions": "off",
     "no-console": "off",
   },
-  "globals": {
-    "document": true,
-    "window": true,
+  "env": {
+    "node": true,
+    "browser": true,
   },
 };
