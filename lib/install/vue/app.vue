3b19e6658f2e954995e25eae64e9d17ccb484ced
ytbryan <ytbryan@gmail.com>
2017-02-24 20:49:56 +0800

    Edit app.vue and hello_vue.js to render only Hello Vue!

--- lib/install/vue/app.vue
+++ lib/install/vue/app.vue
@@ -1,23 +1,5 @@
 <template>
-  <div id="app" class="container">
-    <div class="row">
-      <div class="col-sm-6">
-        <h1>Todos</h1>
-        <todo-list></todo-list>
-      </div>
-      <div class="col-sm-6">
-        <img src="./assets/logo.png" alt="Vue logo">
-      </div>
-    </div>
+  <div id="app">
+    Hello Vue!
   </div>
 </template>
-
-<script>
-  import TodoList from './components/todo-list.vue'
-
-  export default {
-    components: {
-      TodoList
-    }
-  }
-</script>
