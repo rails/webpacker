5721c5d20b8f1ab1686081906f9e7eab5752b80b
Anton Maminov <anton.linux@gmail.com>
2017-02-26 14:11:06 +0200

    remove version and change Vue component

--- lib/install/vue/app.vue
+++ lib/install/vue/app.vue
@@ -1,5 +1,22 @@
 <template>
   <div id="app">
-    Hello Vue!
+    <p>{{ message }}</p>
   </div>
 </template>
+
+<script>
+module.exports = {
+  data: function () {
+    return {
+      message: "Hello Vue!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
