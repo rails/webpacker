ead8736093bd156abf618b6c4a09ae1106ad775a
Long Nguyen <long.polyglot@gmail.com>
2017-04-21 08:43:54 +0700

    Code improve

--- test/env_test.rb
+++ test/env_test.rb
@@ -2,15 +2,15 @@ require "webpacker_test"
 
 class EnvTest < Minitest::Test
   def test_current_env
-    assert Webpacker::Env.current == "production"
+    assert_equal Webpacker::Env.current, "production"
   end
 
   def test_env_is_development?
-    refute Webpacker::Env.development?
+    refute_predicate Webpacker::Env, :development?
   end
 
   def test_file_path
     correct_path = File.join(File.dirname(__FILE__), "config", "webpack", "paths.yml").to_s
-    assert Webpacker::Env.file_path.to_s == correct_path
+    assert_equal Webpacker::Env.file_path.to_s, correct_path
   end
 end
