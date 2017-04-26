3bc2baa76638aeedc2c852e673d980ed07681b6e
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-04-26 13:18:44 +0100

    Include webpacker helper to action view #172

--- lib/webpacker/railtie.rb
+++ lib/webpacker/railtie.rb
@@ -8,6 +8,10 @@ class Webpacker::Engine < ::Rails::Engine
       ActionController::Base.helper Webpacker::Helper
     end
 
+    ActiveSupport.on_load :action_view do
+      include Webpacker::Helper
+    end
+
     Webpacker.bootstrap
     Spring.after_fork {  Webpacker.bootstrap } if defined?(Spring)
   end
