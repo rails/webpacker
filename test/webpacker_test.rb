4bb68e13008a95077d8b1b0cd197b6b1a267cdb0
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-05-01 21:54:00 +0100

    Add test for caching

--- test/webpacker_test.rb
+++ test/webpacker_test.rb
@@ -5,9 +5,16 @@ require "rails"
 require "rails/test_help"
 require "webpacker"
 
+class WebpackerTest < Minitest::Test
+  def test_caching
+    assert_equal Webpacker.caching, false
+  end
+end
+
 module TestApp
   class Application < ::Rails::Application
     config.root = File.join(File.dirname(__FILE__), "test_app")
+    config.eager_load = false
   end
 end
 
