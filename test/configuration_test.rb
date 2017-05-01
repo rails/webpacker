69c09a51011bca67ff75e544b6a724d441b0018c
Long Nguyen <long.polyglot@gmail.com>
2017-05-02 02:00:50 +0700

    Add more tests (#299)
    * Add configuration tests
    
    * Add manifest tests
    
    * Add helper tests
    
    * Add missing secret_key_base for test env

---
+++ test/configuration_test.rb
@@ -0,0 +1,42 @@
+require "webpacker_test"
+
+class ConfigurationTest < Minitest::Test
+  def test_config_path
+    config_path = File.join(File.dirname(__FILE__), "test_app/config", "webpack").to_s
+    assert_equal Webpacker::Configuration.config_path.to_s, config_path
+  end
+
+  def test_entry_path
+    entry_path = File.join(File.dirname(__FILE__), "test_app/app/javascript", "packs").to_s
+    assert_equal Webpacker::Configuration.entry_path.to_s, entry_path
+  end
+
+  def test_file_path
+    file_path = File.join(File.dirname(__FILE__), "test_app/config", "webpack", "paths.yml").to_s
+    assert_equal Webpacker::Configuration.file_path.to_s, file_path
+  end
+
+  def test_manifest_path
+    manifest_path = File.join(File.dirname(__FILE__), "test_app/public/packs", "manifest.json").to_s
+    assert_equal Webpacker::Configuration.manifest_path.to_s, manifest_path
+  end
+
+  def test_packs_path
+    packs_path = File.join(File.dirname(__FILE__), "test_app/public/packs").to_s
+    assert_equal Webpacker::Configuration.packs_path.to_s, packs_path
+  end
+
+  def test_output_path
+    output_path = File.join(File.dirname(__FILE__), "test_app/public").to_s
+    assert_equal Webpacker::Configuration.output_path.to_s, output_path
+  end
+
+  def test_source
+    assert_equal Webpacker::Configuration.source.to_s, "app/javascript"
+  end
+
+  def test_source_path
+    source_path = File.join(File.dirname(__FILE__), "test_app/app/javascript").to_s
+    assert_equal Webpacker::Configuration.source_path.to_s, source_path
+  end
+end
