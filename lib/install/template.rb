2fd3112bf2625325d66e2caeb2f0f17b64022759
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-05-06 13:49:15 +0100

    Add url parsing for asset host
    Fix asset host duplication
    
    Revert change in shared.js
    
    Return if no host is provided
    
    Remove https
    
    Remove asset host
    
    Change function name

--- lib/install/template.rb
+++ lib/install/template.rb
@@ -27,7 +27,7 @@ run "yarn add webpack webpack-merge js-yaml path-complete-extname " \
 "webpack-manifest-plugin babel-loader@7.x coffee-loader coffee-script " \
 "babel-core babel-preset-env compression-webpack-plugin rails-erb-loader glob " \
 "extract-text-webpack-plugin node-sass file-loader sass-loader css-loader style-loader " \
-"postcss-loader autoprefixer postcss-smart-import precss"
+"postcss-loader autoprefixer postcss-smart-import precss url-parse"
 
 puts "Installing dev server for live reloading"
 run "yarn add --dev webpack-dev-server"
