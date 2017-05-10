9b40a293c037354a09222a5525dfddf3a251d726
Brooke Kuhlmann <bkuhlmann@users.noreply.github.com>
2017-05-10 10:47:05 -0600

    Fixed Elm install issue with older Webpacker gem versions. (#356)
    * Refactored Elm install script grouping of operations.
    
    - Organizes the script so that there is a logical grouping of
      operations (this'll have more relevanace in the next commit):
      - copying
      - installing
      - updating
    
    * Updated Elm install script to update `paths.yml`.
    
    - Resolves an edge case where one could have installed an older version
      of this gem, upgrade, and then run `rails webpacker:install:elm` only
      realize that the `paths.yml` file wasn't updated because the Elm
      extension was only added in a new version of this gem.
    - Ensures the Elm script is self-contained and all modifications needed
      to support Elm via Webpack is within this script only.
    
    * Removed Elm extension from `paths.yml`.
    
    - Now that the Elm script has the knowledge to update this file
      appropriately (see previous commit), this entry no longer needs to
      exist as it'll be dynamically added during Elm install script
      execution.

--- lib/install/elm.rb
+++ lib/install/elm.rb
@@ -11,16 +11,19 @@ puts "Copying elm app file to #{Webpacker::Configuration.entry_path}"
 copy_file "#{__dir__}/examples/elm/hello_elm.js",
           "#{Webpacker::Configuration.entry_path}/hello_elm.js"
 
-puts "Updating .gitignore to include elm-stuff folder"
-insert_into_file ".gitignore", "/elm-stuff\n", before: "/node_modules\n"
-
 puts "Installing all elm dependencies"
 exec "#{RbConfig.ruby} ./bin/yarn add elm"
 exec "#{RbConfig.ruby} ./bin/yarn add --dev elm-hot-loader elm-webpack-loader"
 run "yarn run elm package install -- --yes"
 
+puts "Updating Webpack paths to include Elm file extension"
+insert_into_file Webpacker::Configuration.file_path, "    - .elm\n", after: /extensions:\n/
+
 puts "Updating elm source location"
 source_path = File.join Webpacker::Configuration.source, Webpacker::Configuration.paths.fetch(:entry, "packs")
 gsub_file "elm-package.json", /\"\.\"\n/, %("#{source_path}"\n)
+
+puts "Updating .gitignore to include elm-stuff folder"
+insert_into_file ".gitignore", "/elm-stuff\n", before: "/node_modules\n"
 
 puts "Webpacker now supports elm 🎉"
