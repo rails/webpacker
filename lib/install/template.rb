a0c1e9a7f2f3c99226925dffae03f1ea04f902db
Adrian Stainforth <djforth@users.noreply.github.com>
2017-05-26 15:44:26 +0100

    Add cssnext and remove PreCSS (#344)
    * Remove precss(plus autoprefixer as included in cssnext) and upgrade to cssnext

--- lib/install/template.rb
+++ lib/install/template.rb
@@ -31,7 +31,7 @@ run "yarn add webpack webpack-merge js-yaml path-complete-extname " \
 "webpack-manifest-plugin babel-loader@7.x coffee-loader coffee-script " \
 "babel-core babel-preset-env babel-polyfill compression-webpack-plugin rails-erb-loader glob " \
 "extract-text-webpack-plugin node-sass file-loader sass-loader css-loader style-loader " \
-"postcss-loader autoprefixer postcss-smart-import precss resolve-url-loader " \
+"postcss-loader postcss-cssnext postcss-smart-import resolve-url-loader " \
 "babel-plugin-syntax-dynamic-import babel-plugin-transform-class-properties"
 
 puts "Installing dev server for live reloading"
