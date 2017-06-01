6e3656bfc93362bd5d5214799944320a9738a751
Alex Mouton <thomas.mouton@gmail.com>
2017-06-01 11:36:59 -0700

    Added typed asset_path_pack versions for consistency with the link tag brethren. Separated pack asset name from link helpers.

--- lib/webpacker/helper.rb
+++ lib/webpacker/helper.rb
@@ -11,6 +11,21 @@ module Webpacker::Helper
   def asset_pack_path(name, **options)
     asset_path(Webpacker::Manifest.lookup(name), **options)
   end
+
+  # Computes the full path for a given webpacker javascript asset.
+  # Return relative path using manifest.json and passes it to asset_url helper
+  # This will use asset_path internally, so most of their behaviors will be the same.
+  def javascript_pack_path(name, **options)
+    asset_path(asset_path(name, :javascript), **options)
+  end
+
+  # Computes the full path for a given webpacker stylesheet asset.
+  # Return relative path using manifest.json and passes it to asset_url helper
+  # This will use asset_path internally, so most of their behaviors will be the same.
+  def stylesheet_pack_path(name, **options)
+    asset_path(asset_path(name, :stylesheet), **options)
+  end
+
   # Creates a script tag that references the named pack file, as compiled by Webpack per the entries list
   # in config/webpack/shared.js. By default, this list is auto-generated to match everything in
   # app/javascript/packs/*.js. In production mode, the digested reference is automatically looked up.
@@ -25,7 +40,7 @@ module Webpacker::Helper
   #   <%= javascript_pack_tag 'calendar', 'data-turbolinks-track': 'reload' %> # =>
   #   <script src="/packs/calendar-1016838bab065ae1e314.js" data-turbolinks-track="reload"></script>
   def javascript_pack_tag(name, **options)
-    javascript_include_tag(Webpacker::Manifest.lookup("#{name}#{compute_asset_extname(name, type: :javascript)}"), **options)
+    javascript_include_tag(asset_path(name, :javascript), **options)
   end
 
   # Creates a link tag that references the named pack file, as compiled by Webpack per the entries list
@@ -42,6 +57,16 @@ module Webpacker::Helper
   #   <%= stylesheet_pack_tag 'calendar', 'data-turbolinks-track': 'reload' %> # =>
   #   <link rel="stylesheet" media="screen" href="/packs/calendar-1016838bab065ae1e122.css" data-turbolinks-track="reload" />
   def stylesheet_pack_tag(name, **options)
-    stylesheet_link_tag(Webpacker::Manifest.lookup("#{name}#{compute_asset_extname(name, type: :stylesheet)}"), **options)
+    stylesheet_link_tag(asset_path(name, :stylesheet), **options)
+  end
+
+  private
+
+  def asset_path(name, type)
+    Webpacker::Manifest.lookup(asset_name(name, type))
+  end
+
+  def asset_name(name, type)
+    "#{name}#{compute_asset_extname(name, type: type)}"
   end
 end
