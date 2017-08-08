b6af4f6299a1d229744833b74e89248dc061b1af
David Heinemeier Hansson <david@loudthinking.com>
2017-08-08 16:38:13 -0500

    Move comments down to the class and expand on on-demand compilation

--- lib/webpacker/manifest.rb
+++ lib/webpacker/manifest.rb
@@ -1,12 +1,16 @@
-# Singleton registry for accessing the packs path using generated manifest.
-# This allows javascript_pack_tag, stylesheet_pack_tag, asset_pack_path to take a reference to,
-# say, "calendar.js" or "calendar.css" and turn it into "/packs/calendar.js" or
-# "/packs/calendar.css" in development. In production mode, it returns compiles
-# files, # "/packs/calendar-1016838bab065ae1e314.js" and
-# "/packs/calendar-1016838bab065ae1e314.css" for long-term caching
-
 require "webpacker/file_loader"
 
+# Singleton registry for accessing the packs path using a generated manifest.
+# This allows javascript_pack_tag, stylesheet_pack_tag, asset_pack_path to take a reference to,
+# say, "calendar.js" or "calendar.css" and turn it into "/packs/calendar.js" or
+# "/packs/calendar.css" in development.
+#
+# In production mode, it returns compiles files, like
+# "/packs/calendar-1016838bab065ae1e314.js" and "/packs/calendar-1016838bab065ae1e314.css",
+# for long-term caching.
+#
+# When the configuration is set to on-demand compilation, with the `compile: true` option in
+# the webpacker.yml file, any lookups will be preceeded by a compilation if one is needed.
 class Webpacker::Manifest < Webpacker::FileLoader
   class << self
     def file_path
