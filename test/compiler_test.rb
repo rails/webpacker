8c28fd5f6e0e57e253241402562237498d7e3402
Javan Makhmali <javan@javan.us>
2017-08-12 11:49:31 -0400

    Fix compiler test

--- test/compiler_test.rb
+++ test/compiler_test.rb
@@ -2,7 +2,9 @@ require "webpacker_test_helper"
 
 class CompilerTest < Minitest::Test
   def setup
-    Webpacker.compiler.send(:compilation_timestamp_path).delete
+    Webpacker.compiler.send(:compilation_timestamp_path).tap do |path|
+      path.delete if path.exist?
+    end
   end
 
   def test_freshness
