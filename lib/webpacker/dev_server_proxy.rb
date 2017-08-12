e5b7def2e96037b338c1abda087827c68ffb6793
David Heinemeier Hansson <david@loudthinking.com>
2017-08-12 14:18:25 -0500

    Only proxy to dev server if its running

--- lib/webpacker/dev_server_proxy.rb
+++ lib/webpacker/dev_server_proxy.rb
@@ -9,7 +9,7 @@ class Webpacker::DevServerProxy < Rack::Proxy
   end
 
   def perform_request(env)
-    if env["PATH_INFO"] =~ /#{Webpacker.config.public_output_dir}/
+    if env["PATH_INFO"] =~ /#{Webpacker.config.public_output_dir}/ && Webpacker.dev_server.running?
       env["HTTP_HOST"] = "#{Webpacker.dev_server.host}:#{Webpacker.dev_server.port}"
       super(env)
     else
