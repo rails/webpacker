9308b6117f47d0bed1e3f0025606a9a599075ecb
David Heinemeier Hansson <david@loudthinking.com>
2017-08-12 14:34:49 -0500

    Not needed

--- test/webpacker_test_helper.rb
+++ test/webpacker_test_helper.rb
@@ -17,7 +17,6 @@ module TestApp
   class Application < ::Rails::Application
     config.root = File.join(File.dirname(__FILE__), "test_app")
     config.eager_load = true
-    ::Rails.env = ENV["RAILS_ENV"] = "test"
   end
 end
 
