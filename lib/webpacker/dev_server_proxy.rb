3e9ae23fea49fd5e87514d96d096d1265300d980
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-08-12 10:55:43 +0100

    Add and mount the dev_server proxy
    Move dev-server to defaults

---
+++ lib/webpacker/dev_server_proxy.rb
@@ -0,0 +1,19 @@
+require "rack/proxy"
+
+class Webpacker::DevServerProxy < Rack::Proxy
+  def rewrite_response(response)
+    status, headers, body = response
+    headers.delete "transfer-encoding"
+    headers.delete "content-length" if Webpacker.dev_server.https?
+    response
+  end
+
+  def perform_request(env)
+    if env["PATH_INFO"] =~ /#{Webpacker.config.public_output_dir}/
+      env["HTTP_HOST"] = "#{Webpacker.dev_server.host}:#{Webpacker.dev_server.port}"
+      super(env)
+    else
+      @app.call(env)
+    end
+  end
+end
--- lib/webpacker/railtie.rb
+++ lib/webpacker/railtie.rb
@@ -1,9 +1,16 @@
 require "rails/railtie"
 
 require "webpacker/helper"
+require "webpacker/dev_server_proxy"
 
 class Webpacker::Engine < ::Rails::Engine
   initializer "webpacker.helper" do |app|
+    if Rails::VERSION::MAJOR >= 5
+      app.middleware.insert_before 0, Webpacker::DevServerProxy, ssl_verify_none: true
+    else
+      config.middleware.insert_before 0, "Webpacker::DevServerProxy", ssl_verify_none: true
+    end
+
     ActiveSupport.on_load :action_controller do
       ActionController::Base.helper Webpacker::Helper
     end
