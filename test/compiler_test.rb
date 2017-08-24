faa0c2d42fc24b3e258fe08c4d774ce1b270109a
Gaurav Tiwari <gauravtiwari@users.noreply.github.com>
2017-08-24 20:13:01 +0100

    Use sha digest to track recompilation (#684)

--- test/compiler_test.rb
+++ test/compiler_test.rb
@@ -2,7 +2,7 @@ require "webpacker_test_helper"
 
 class CompilerTest < Minitest::Test
   def setup
-    Webpacker.compiler.send(:compilation_timestamp_path).tap do |path|
+    Webpacker.compiler.send(:compilation_digest_path).tap do |path|
       path.delete if path.exist?
     end
   end
