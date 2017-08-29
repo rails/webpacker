eca739ab16b3d3cc0650c0698e58f20031c503ab
Javan Makhmali <javan@javan.us>
2017-08-29 15:16:44 -0400

    Export an environment instance *and* the base class
    Mostly so we're not locked in to exporting a single value

--- node_module/index.js
+++ node_module/index.js
@@ -1,15 +1,15 @@
 /* eslint global-require: 0 */
 /* eslint import/no-dynamic-require: 0 */
 
+const Environment = require('./environment')
 const { existsSync } = require('fs')
 
-function createEnvironmentInstance() {
-  let path = `./environments/${process.env.NODE_ENV}`
-  if (!existsSync(path)) {
-    path = './environment'
-  }
-  const constructor = require(path)
+function createEnvironment() {
+  const path = `./environments/${process.env.NODE_ENV}`
+  const constructor = existsSync(path) ? require(path) : Environment
   return new constructor()
 }
 
-module.exports = createEnvironmentInstance()
+const environment = createEnvironment()
+
+module.exports = { environment, Environment }
