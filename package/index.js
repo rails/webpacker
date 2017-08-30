df21faead2b427c56152eca744f12ce8fcc1e0ca
Javan Makhmali <javan@javan.us>
2017-08-29 20:17:26 -0400

    Resolve full file path + name to fix existsSync check

--- package/index.js
+++ package/index.js
@@ -2,10 +2,11 @@
 /* eslint import/no-dynamic-require: 0 */
 
 const Environment = require('./environment')
+const { resolve } = require('path')
 const { existsSync } = require('fs')
 
 function createEnvironment() {
-  const path = `./environments/${process.env.NODE_ENV}`
+  const path = resolve(__dirname, 'environments', `${process.env.NODE_ENV}.js`)
   const constructor = existsSync(path) ? require(path) : Environment
   return new constructor()
 }
