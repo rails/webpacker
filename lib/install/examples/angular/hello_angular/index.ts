7e60ec03139dcfe67fb5dc03a8d57cb4ebf574d8
PatrickJS <github@gdi2290.com>
2017-08-31 11:15:33 -0700

    feat(angular): bootstrap on document loaded (#712)

--- lib/install/examples/angular/hello_angular/index.ts
+++ lib/install/examples/angular/hello_angular/index.ts
@@ -3,4 +3,6 @@ import './polyfills.ts';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+document.addEventListener('DOMContentLoaded', () => {
+  platformBrowserDynamic().bootstrapModule(AppModule);
+});
