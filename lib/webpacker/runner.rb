265387652d756ce5c4be31e8981ebddfbe1a62c0
Ross Kaffenberger <rosskaff@gmail.com>
2017-09-19 11:28:01 -0400

    Fix webpack runner

--- lib/webpacker/runner.rb
+++ lib/webpacker/runner.rb
@@ -11,9 +11,9 @@ module Webpacker
     def initialize(argv)
       @argv = argv
 
-      @app_path          = File.expand_path("../", __dir__)
+      @app_path          = File.expand_path(".", Dir.pwd)
       @node_modules_path = File.join(@app_path, "node_modules")
-      @webpack_config    = File.join(@app_path, "config/webpack/#{NODE_ENV}.js")
+      @webpack_config    = File.join(@app_path, "config/webpack/#{ENV["NODE_ENV"]}.js")
 
       unless File.exist?(@webpack_config)
         puts "Webpack config #{@webpack_config} not found, please run 'bundle exec rails webpacker:install' to install webpacker with default configs or add the missing config file for your custom environment."
@@ -31,4 +31,3 @@ module Webpacker
     end
   end
 end
-
