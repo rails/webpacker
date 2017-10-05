fee8af5865c2d103daa50345d22028e1d074060c
Jacob Dam <ngocphuc@gmail.com>
2017-10-05 13:45:21 +0700

    Fix proxy scheme

--- lib/webpacker/dev_server_proxy.rb
+++ lib/webpacker/dev_server_proxy.rb
@@ -11,6 +11,9 @@ class Webpacker::DevServerProxy < Rack::Proxy
   def perform_request(env)
     if env["PATH_INFO"] =~ /#{public_output_uri_path}/ && Webpacker.dev_server.running?
       env["HTTP_HOST"] = env["HTTP_X_FORWARDED_HOST"] = env["HTTP_X_FORWARDED_SERVER"] = Webpacker.dev_server.host_with_port
+      env.delete("HTTPS")
+      env.delete("HTTP_X_FORWARDED_SSL")
+      env["HTTP_X_FORWARDED_PROTO"] = env["HTTP_X_FORWARDED_SCHEME"] = Webpacker.dev_server.protocol
 
       super(env)
     else
