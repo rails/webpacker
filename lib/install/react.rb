398703f33b63bcebc3528c19bb9cad6c67ded5ac
Yoshiyuki Hirano <yhirano@me.com>
2017-10-09 04:32:42 +0900

    Use `say` instead of `puts`

--- lib/install/react.rb
+++ lib/install/react.rb
@@ -8,21 +8,21 @@ if File.exist?(babelrc)
 
   unless react_babelrc["presets"].include?("react")
     react_babelrc["presets"].push("react")
-    puts "Copying react preset to your .babelrc file"
+    say "Copying react preset to your .babelrc file"
 
     File.open(babelrc, "w") do |f|
       f.puts JSON.pretty_generate(react_babelrc)
     end
   end
 else
-  puts "Copying .babelrc to app root directory"
+  say "Copying .babelrc to app root directory"
   copy_file "#{__dir__}/examples/react/.babelrc", ".babelrc"
 end
 
-puts "Copying react example entry file to #{Webpacker.config.source_entry_path}"
+say "Copying react example entry file to #{Webpacker.config.source_entry_path}"
 copy_file "#{__dir__}/examples/react/hello_react.jsx", "#{Webpacker.config.source_entry_path}/hello_react.jsx"
 
-puts "Installing all react dependencies"
+say "Installing all react dependencies"
 run "yarn add react react-dom babel-preset-react prop-types"
 
-puts "Webpacker now supports react.js 🎉"
+say "Webpacker now supports react.js 🎉", :green
