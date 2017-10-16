72bc021f9bb5ccaf44971296fab1b7abf6afa692
yhirano55 <yhirano@me.com>
2017-10-16 17:16:59 +0900

    Fix HelperTest on edge-rails (#952)

--- test/helper_test.rb
+++ test/helper_test.rb
@@ -1,39 +1,44 @@
 require "test_helper"
 
 class HelperTest < ActionView::TestCase
+  tests Webpacker::Helper
+
+  attr_reader :request
+
   def setup
-    @view = ActionView::Base.new
-    @view.extend Webpacker::Helper
+    @request = Class.new do
+      def send_early_hints(links) end
+    end.new
   end
 
   def test_asset_pack_path
-    assert_equal @view.asset_pack_path("bootstrap.js"), "/packs/bootstrap-300631c4f0e0f9c865bc.js"
-    assert_equal @view.asset_pack_path("bootstrap.css"), "/packs/bootstrap-c38deda30895059837cf.css"
+    assert_equal "/packs/bootstrap-300631c4f0e0f9c865bc.js", asset_pack_path("bootstrap.js")
+    assert_equal "/packs/bootstrap-c38deda30895059837cf.css", asset_pack_path("bootstrap.css")
   end
 
   def test_javascript_pack_tag
     assert_equal \
       %(<script src="/packs/bootstrap-300631c4f0e0f9c865bc.js"></script>),
-      @view.javascript_pack_tag("bootstrap.js")
+      javascript_pack_tag("bootstrap.js")
   end
 
   def test_javascript_pack_tag_splat
     assert_equal \
-    %(<script src="/packs/bootstrap-300631c4f0e0f9c865bc.js" defer="defer"></script>\n) +
+      %(<script src="/packs/bootstrap-300631c4f0e0f9c865bc.js" defer="defer"></script>\n) +
         %(<script src="/packs/application-k344a6d59eef8632c9d1.js" defer="defer"></script>),
-      @view.javascript_pack_tag("bootstrap.js", "application.js", defer: true)
+      javascript_pack_tag("bootstrap.js", "application.js", defer: true)
   end
 
   def test_stylesheet_pack_tag
     assert_equal \
       %(<link rel="stylesheet" media="screen" href="/packs/bootstrap-c38deda30895059837cf.css" />),
-      @view.stylesheet_pack_tag("bootstrap.css")
+      stylesheet_pack_tag("bootstrap.css")
   end
 
   def test_stylesheet_pack_tag_splat
     assert_equal \
       %(<link rel="stylesheet" media="all" href="/packs/bootstrap-c38deda30895059837cf.css" />\n) +
         %(<link rel="stylesheet" media="all" href="/packs/application-dd6b1cd38bfa093df600.css" />),
-      @view.stylesheet_pack_tag("bootstrap.css", "application.css", media: "all")
+      stylesheet_pack_tag("bootstrap.css", "application.css", media: "all")
   end
 end
