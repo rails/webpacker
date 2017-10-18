3b09f788ec7f55939b9b26d5feac870c2f18bf03
簡煒航 (Jian Weihang) <tonytonyjan@gmail.com>
2017-10-18 16:33:40 +0800

    Fix proxy condition statement to prevent proxying wrong requests to dev server. (#961)

--- lib/webpacker/dev_server_proxy.rb
+++ lib/webpacker/dev_server_proxy.rb
@@ -9,7 +9,7 @@ class Webpacker::DevServerProxy < Rack::Proxy
   end
 
   def perform_request(env)
-    if env["PATH_INFO"] =~ /#{public_output_uri_path}/ && Webpacker.dev_server.running?
+    if env["PATH_INFO"].start_with?("/#{public_output_uri_path}") && Webpacker.dev_server.running?
       env["HTTP_HOST"] = env["HTTP_X_FORWARDED_HOST"] = env["HTTP_X_FORWARDED_SERVER"] = Webpacker.dev_server.host_with_port
 
       super(env)
