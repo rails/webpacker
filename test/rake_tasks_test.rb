8929e14564ebb093a158745305a629c4220f54f1
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2017-11-15 16:49:34 +0530

    Remove check for webpacker binstubs. (Fixes #988)

--- test/rake_tasks_test.rb
+++ test/rake_tasks_test.rb
@@ -4,7 +4,6 @@ class RakeTasksTest < Minitest::Test
   def test_rake_tasks
     output = Dir.chdir(test_app_path) { `rake -T` }
     assert_includes output, "webpacker"
-    assert_includes output, "webpacker:check_binstubs"
     assert_includes output, "webpacker:check_node"
     assert_includes output, "webpacker:check_yarn"
     assert_includes output, "webpacker:clobber"
@@ -15,11 +14,6 @@ class RakeTasksTest < Minitest::Test
     assert_includes output, "webpacker:install:react"
     assert_includes output, "webpacker:install:vue"
     assert_includes output, "webpacker:verify_install"
-  end
-
-  def test_rake_task_webpacker_check_binstubs
-    output = Dir.chdir(test_app_path) { `rake webpacker:check_binstubs 2>&1` }
-    refute_includes output, "webpack binstubs not found."
   end
 
   private
