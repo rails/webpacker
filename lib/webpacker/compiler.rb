f01102a17f85923e03dad6cac358898ee4369047
Yunlei Liu <yunlei.liu@appfolio.com>
2018-02-24 11:21:36 -0800

    add relative_url_root to webpacker config (#1236)

--- lib/webpacker/compiler.rb
+++ lib/webpacker/compiler.rb
@@ -78,6 +78,8 @@ class Webpacker::Compiler
     end
 
     def webpack_env
-      env.merge("NODE_ENV" => @webpacker.env, "WEBPACKER_ASSET_HOST" => ActionController::Base.helpers.compute_asset_host)
+      env.merge("NODE_ENV"                    => @webpacker.env,
+                "WEBPACKER_ASSET_HOST"        => ActionController::Base.helpers.compute_asset_host,
+                "WEBPACKER_RELATIVE_URL_ROOT" => ActionController::Base.relative_url_root)
     end
 end
