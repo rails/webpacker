d0ecdf43ac76e2094ca08b2a6431a628d2c05307
Jakob Pupke <jakob.pupke@gmail.com>
2018-03-14 12:11:09 +0100

    added test for custom environment

--- package/__tests__/config.js
+++ package/__tests__/config.js
@@ -1,28 +1,33 @@
 /* global test expect, describe */
 
 const { chdirTestApp, chdirCwd } = require('../utils/helpers')
+const EXPECTED_EXTENSIONS = [
+  '.js',
+  '.sass',
+  '.scss',
+  '.css',
+  '.module.sass',
+  '.module.scss',
+  '.module.css',
+  '.png',
+  '.svg',
+  '.gif',
+  '.jpeg',
+  '.jpg'
+]
 
 chdirTestApp()
 
-const config = require('../config')
-
 describe('Config', () => {
+  beforeEach(() => jest.resetModules())
   afterAll(chdirCwd)
 
   test('should return extensions as listed in app config', () => {
-    expect(config.extensions).toEqual([
-      '.js',
-      '.sass',
-      '.scss',
-      '.css',
-      '.module.sass',
-      '.module.scss',
-      '.module.css',
-      '.png',
-      '.svg',
-      '.gif',
-      '.jpeg',
-      '.jpg'
-    ])
+    expect(require('../config').extensions).toEqual(EXPECTED_EXTENSIONS)
+  })
+
+  test('should return extensions as listed in app config for custom environment', () => {
+    process.env.NODE_ENV = 'qa2'
+    expect(require('../config').extensions).toEqual(EXPECTED_EXTENSIONS)
   })
 })
