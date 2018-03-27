f8df1ab63e571c23a413fabd9eb4e05f0b187a8d
app2641 <app2641@gmail.com>
2018-03-27 11:24:43 +0900

    Fix incorrect assignment

--- lib/webpacker.rb
+++ lib/webpacker.rb
@@ -15,7 +15,7 @@ module Webpacker
 
   def with_node_env(env)
     original = ENV["NODE_ENV"]
-    ENV["NODE_ENV"] = "production"
+    ENV["NODE_ENV"] = env
     yield
   ensure
     ENV["NODE_ENV"] = original
