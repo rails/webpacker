6d3067171c7048b230e995230a2b829bdd1269ad
Jordan Pickwell <jpickwell@users.noreply.github.com>
2018-04-17 16:43:16 -0500

    Include `RAILS_RELATIVE_URL_ROOT` environment variable in `publicPath`. (#1428)

--- package/__tests__/config.js
+++ package/__tests__/config.js
@@ -1,13 +1,29 @@
 /* global test expect, describe */
 
-const { chdirTestApp, chdirCwd } = require('../utils/helpers')
+const { chdirCwd, chdirTestApp } = require('../utils/helpers')
 
 chdirTestApp()
 
 const config = require('../config')
 
 describe('Config', () => {
+  beforeEach(() => jest.resetModules())
   afterAll(chdirCwd)
+
+  test('public path', () => {
+    process.env.RAILS_ENV = 'development'
+    delete process.env.RAILS_RELATIVE_URL_ROOT
+    const config = require('../config')
+    expect(config.publicPath).toEqual('/packs/')
+  })
+
+  // also tests removal of extra slashes
+  test('public path with relative root', () => {
+    process.env.RAILS_ENV = 'development'
+    process.env.RAILS_RELATIVE_URL_ROOT = '/foo'
+    const config = require('../config')
+    expect(config.publicPath).toEqual('/foo/packs/')
+  })
 
   test('should return extensions as listed in app config', () => {
     expect(config.extensions).toEqual([
