4d2e12a9d448c6610b17784f1f32bc46ab6d8605
Tobias L. Maier <me@tobiasmaier.info>
2018-04-26 06:44:14 +1000

    Do not set NODE_PATH env anymore
    Seems to be ignored by webpack and is also discouraged to use
    
    Ref. https://github.com/webpack/webpack/issues/4479#issuecomment-286701441

--- lib/webpacker/webpack_runner.rb
+++ lib/webpacker/webpack_runner.rb
@@ -4,7 +4,6 @@ require "webpacker/runner"
 module Webpacker
   class WebpackRunner < Webpacker::Runner
     def run
-      env = { "NODE_PATH" => @node_modules_path.shellescape }
       cmd = [ "#{@node_modules_path}/.bin/webpack", "--config", @webpack_config ] + @argv
 
       Dir.chdir(@app_path) do
