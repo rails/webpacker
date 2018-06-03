f3474142eee19ac7c4fdab2aa286cfa3d1b5fab3
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2018-06-03 15:16:13 +0100

    Fix uglify options and add css asset optimisation

--- package/environments/production.js
+++ package/environments/production.js
@@ -1,5 +1,6 @@
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const CompressionPlugin = require('compression-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const Base = require('./base')
 
 module.exports = class extends Base {
@@ -15,6 +16,8 @@ module.exports = class extends Base {
       })
     )
 
+    this.plugins.append('OptimizeCSSAssets', new OptimizeCSSAssetsPlugin())
+
     this.config.merge({
       devtool: 'nosources-source-map',
       stats: 'normal',
@@ -26,8 +29,13 @@ module.exports = class extends Base {
             cache: true,
             sourceMap: true,
             uglifyOptions: {
-              ecma: 5,
+              parse: {
+                // Let uglify-js parse ecma 8 code but always output
+                // ES5 compliant code for older browsers
+                ecma: 8
+              },
               compress: {
+                ecma: 5,
                 warnings: false,
                 comparisons: false
               },
@@ -35,6 +43,7 @@ module.exports = class extends Base {
                 safari10: true
               },
               output: {
+                ecma: 5,
                 comments: false,
                 ascii_only: true
               }
