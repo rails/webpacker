3b68afa799be18bc40748b318d48fea085b19422
Lachlan Sylvester <lachlan.sylvester@publicisfrontfoot.com.au>
2018-08-24 12:34:41 +1000

    move into the app root in binstubs, use full path to binstub in compiler

--- lib/webpacker/compiler.rb
+++ lib/webpacker/compiler.rb
@@ -66,7 +66,7 @@ class Webpacker::Compiler
     def run_webpack
       logger.info "Compiling…"
 
-      stdout, sterr , status = Open3.capture3(webpack_env, "#{RbConfig.ruby} ./bin/webpack")
+      stdout, sterr , status = Open3.capture3(webpack_env, "#{RbConfig.ruby} #{@webpacker.root_path}/bin/webpack")
 
       if status.success?
         logger.info "Compiled all packs in #{config.public_output_path}"
