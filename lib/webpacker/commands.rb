8d5519af470587e6317f4b4c9f42e05b2fe1861c
Eric Boehs <ericboehs@gmail.com>
2018-10-10 21:42:36 -0500

    Clean task: keep the last two versions of the packs

--- lib/webpacker/commands.rb
+++ lib/webpacker/commands.rb
@@ -10,8 +10,18 @@ class Webpacker::Commands
 
     files_on_filesystem = Dir.glob("#{config.public_output_path}/**/*").select { |f| File.file? f }
     files_in_manifest = manifest.refresh.values.map { |f| File.join config.root_path, "public", f }
+
+    count_to_keep = 2
+    versions_to_keep = files_in_manifest.flat_map do |file_in_manifest|
+      file_prefix, file_ext = file_in_manifest.scan(/(.*)[0-9a-f]{20}(.*)/).first
+      versions_of_file = Dir.glob("#{file_prefix}*#{file_ext}").grep(/#{file_prefix}[0-9a-f]{20}#{file_ext}/)
+      versions_of_file.map do |version_of_file|
+        [version_of_file, File.mtime(version_of_file).utc.to_i]
+      end.sort_by(&:last).reverse.first(count_to_keep).map(&:first)
+    end
+
     manifest_file = File.join config.public_output_path, "manifest.json"
-    files_to_be_removed = files_on_filesystem - files_in_manifest - [manifest_file]
+    files_to_be_removed = files_on_filesystem - files_in_manifest - versions_to_keep - [manifest_file]
 
     files_to_be_removed.each { |f| File.delete f }
   end
