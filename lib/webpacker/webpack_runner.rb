c80a968fd6d2c747ed427b420502902a4bec9c97
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-11-02 13:37:26 +0900

    Run commands via `yarn` when not exist `node_modules`
    Currently, commands(`webpack` and `webpacker-dev-server`) are need to exist
    under the `node_modules/.bin` directory.
    
    But when Yarn Plug'n'Play using, `node_modules/.bin` is not created.
    Ref: https://github.com/yarnpkg/rfcs/blob/master/accepted/0000-plug-an-play.md
    
    In this case, need to execute the command via `yarn`.
    So if `node_modules` does not exist, executes the command via `yarn`.

--- lib/webpacker/webpack_runner.rb
+++ lib/webpacker/webpack_runner.rb
@@ -5,11 +5,22 @@ module Webpacker
   class WebpackRunner < Webpacker::Runner
     def run
       env = Webpacker::Compiler.env
-      cmd = [ "#{@node_modules_bin_path}/webpack", "--config", @webpack_config ] + @argv
+
+      cmd = if node_modules_bin_exist?
+        ["#{@node_modules_bin_path}/webpack"]
+      else
+        ["yarn", "webpack"]
+      end
+      cmd += ["--config", @webpack_config] + @argv
 
       Dir.chdir(@app_path) do
-        exec env, *cmd
+        Kernel.exec env, *cmd
       end
     end
+
+    private
+      def node_modules_bin_exist?
+        File.exist?("#{@node_modules_bin_path}/webpack")
+      end
   end
 end
