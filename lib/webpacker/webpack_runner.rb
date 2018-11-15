4f62a1ae7b729d3de3d36487703e861cb450fc45
Michael S. Kazmier <michael.s.kazmier@uscis.dhs.gov>
2018-11-15 09:29:44 -0700

    fixes rubocop errors

--- lib/webpacker/webpack_runner.rb
+++ lib/webpacker/webpack_runner.rb
@@ -17,14 +17,14 @@ module Webpacker
 
     private
 
-    # config/webpacker.yml may contain ERB that needs to be processed so that javascript can proceed
-    def compile_config
-      config_file = File.join('config', 'webpacker.yml')
-      config = YAML.load(ERB.new(File.read(config_file)).result)[env].deep_symbolize_keys
-      processed_config_file = File.join('config', '.webpacker.yml')
-      File.open(processed_config_file, 'w') do |f|
-        f.write(config.to_yaml)
+      # config/webpacker.yml may contain ERB that needs to be processed so that javascript can proceed
+      def compile_config
+        config_file = File.join("config", "webpacker.yml")
+        config = YAML.load(ERB.new(File.read(config_file)).result)[env].deep_symbolize_keys
+        processed_config_file = File.join("config", ".webpacker.yml")
+        File.open(processed_config_file, "w") do |f|
+          f.write(config.to_yaml)
+        end
       end
-    end
   end
 end
