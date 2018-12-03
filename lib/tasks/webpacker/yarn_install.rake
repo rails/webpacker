0bef289d54d8aacc644c656245099c383e3dc33a
Oliver Peate <odlp@users.noreply.github.com>
2018-12-03 12:37:53 +0000

    Make "webpacker:yarn_install" task env aware (#1331)
    * Make webpacker:yarn_install task env aware
    
    When webpacker compiles in a non-production environment it's removing
    node_modules which are dev dependencies. This is problematic on CI for
    example because these dependencies may be required.
    
    * Use alternative to Bundler.with_clean_env
    
    This was failing on CI presumably because BUNDLE_GEMFILE was being
    lost, causing the 4.2.x Gemfile tests to fail.
    
    * Remove explicit production flag from Yarn Install task
    
    If the NODE_ENV is set then Yarn will behave accordingly (e.g. skip
    devDependencies if NODE_ENV=production).
    
    Based on discussion:
    https://github.com/rails/webpacker/pull/1331#issuecomment-443518808
    
    * Use existing with_node_env helper

--- lib/tasks/webpacker/yarn_install.rake
+++ lib/tasks/webpacker/yarn_install.rake
@@ -1,6 +1,6 @@
 namespace :webpacker do
   desc "Support for older Rails versions. Install all JavaScript dependencies as specified via Yarn"
   task :yarn_install do
-    system "yarn install --no-progress --frozen-lockfile --production"
+    system "yarn install --no-progress"
   end
 end
--- test/rake_tasks_test.rb
+++ test/rake_tasks_test.rb
@@ -22,8 +22,42 @@ class RakeTasksTest < Minitest::Test
     refute_includes output, "webpack binstubs not found."
   end
 
+  def test_rake_webpacker_yarn_install_in_non_production_environments
+    assert_includes test_app_dev_dependencies, "right-pad"
+
+    Webpacker.with_node_env("test") do
+      Dir.chdir(test_app_path) do
+        `bundle exec rake webpacker:yarn_install`
+      end
+    end
+
+    assert_includes installed_node_module_names, "right-pad",
+                    "Expected dev dependencies to be installed"
+  end
+
+  def test_rake_webpacker_yarn_install_in_production_environment
+    Webpacker.with_node_env("production") do
+      Dir.chdir(test_app_path) do
+        `bundle exec rake webpacker:yarn_install`
+      end
+    end
+
+    refute_includes installed_node_module_names, "right-pad",
+                    "Expected only production dependencies to be installed"
+  end
+
   private
     def test_app_path
       File.expand_path("test_app", __dir__)
+    end
+
+    def test_app_dev_dependencies
+      package_json = File.expand_path("package.json", test_app_path)
+      JSON.parse(File.read(package_json))["devDependencies"]
+    end
+
+    def installed_node_module_names
+      node_modules_path = File.expand_path("node_modules", test_app_path)
+      Dir.chdir(node_modules_path) { Dir.glob("*") }
     end
 end
