58cf57fc078512a3bb94046054af7c31701c3d1e
Gaurav Tiwari <gauravtiwari@users.noreply.github.com>
2018-12-14 20:39:30 +0000

    Make everything latest and greatest (#1832)
    Bump gems and packages

--- package/rules/file.js
+++ package/rules/file.js
@@ -2,7 +2,7 @@ const { join } = require('path')
 const { source_path: sourcePath, static_assets_extensions: fileExtensions } = require('../config')
 
 module.exports = {
-  test: new RegExp(`(${fileExtensions.join('|')})`, 'i'),
+  test: new RegExp(`(${fileExtensions.join('|')})$`, 'i'),
   use: [
     {
       loader: 'file-loader',
