f88bbad3ae3aa3e6172e1664e5eca9faa7fbb256
Kostas Pelelis <13784424+kpelelis@users.noreply.github.com>
2019-03-03 18:36:51 +0200

    compiler: Use environmental variables instead of overwrite (#1927)
    * compiler: Use env variables instead of overwrite
    
    Webpacker uses 2 specific environmental variables (WEBPACKER_ASSET_HOST
    and WEBPACKER_RELATIVE_URL) that are set during boot. There is no way
    that this variables can be overwritten. This patch makes use of the
    external variables or falls back to the default behavior.
    
    * Add tests

--- lib/webpacker/compiler.rb
+++ lib/webpacker/compiler.rb
@@ -90,7 +90,7 @@ class Webpacker::Compiler
     def webpack_env
       return env unless defined?(ActionController::Base)
 
-      env.merge("WEBPACKER_ASSET_HOST"        => ActionController::Base.helpers.compute_asset_host,
-                "WEBPACKER_RELATIVE_URL_ROOT" => ActionController::Base.relative_url_root)
+      env.merge("WEBPACKER_ASSET_HOST"        => ENV.fetch("WEBPACKER_ASSET_HOST", ActionController::Base.helpers.compute_asset_host),
+                "WEBPACKER_RELATIVE_URL_ROOT" => ENV.fetch("WEBPACKER_RELATIVE_URL_ROOT", ActionController::Base.relative_url_root))
     end
 end
