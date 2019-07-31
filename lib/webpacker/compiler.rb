2de0d3a526c97e65ce77b7b70aae36368e26fe9f
Samuel Sieg <samuel.sieg@crafted.ch>
2019-07-31 13:24:59 +0200

    Include configured extensions when generating the watched files digest (#2177)

--- lib/webpacker/compiler.rb
+++ lib/webpacker/compiler.rb
@@ -87,7 +87,7 @@ class Webpacker::Compiler
     def default_watched_paths
       [
         *config.resolved_paths_globbed,
-        "#{config.source_path.relative_path_from(config.root_path)}/**/*",
+        config.source_path_globbed,
         "yarn.lock", "package.json",
         "config/webpack/**/*"
       ].freeze
