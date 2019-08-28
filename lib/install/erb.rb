d0fa1d1351bfc5bb53e76b94ce6f33cf2175a879
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-08-28 14:04:59 -0700

    Remove extra space from erb installer (#2258)

--- lib/install/erb.rb
+++ lib/install/erb.rb
@@ -5,7 +5,7 @@ copy_file "#{__dir__}/loaders/erb.js", Rails.root.join("config/webpack/loaders/e
 
 say "Adding erb loader to config/webpack/environment.js"
 insert_into_file Rails.root.join("config/webpack/environment.js").to_s,
-  "const erb =  require('./loaders/erb')\n",
+  "const erb = require('./loaders/erb')\n",
   after: /require\(('|")@rails\/webpacker\1\);?\n/
 
 insert_into_file Rails.root.join("config/webpack/environment.js").to_s,
