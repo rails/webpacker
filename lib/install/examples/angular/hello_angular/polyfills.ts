6772fe276ebbd96bb5f3a15f300a86b9dd63fa1d
Ibrahim Abdullah <abdul.ibrahim16@gmail.com>
2019-09-14 17:20:50 +0200

    fixing angular example app polyfills imports to work with core-js@3

--- lib/install/examples/angular/hello_angular/polyfills.ts
+++ lib/install/examples/angular/hello_angular/polyfills.ts
@@ -38,8 +38,8 @@
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
+import 'core-js/es/reflect';
+import 'core-js/proposals/reflect-metadata';
 
 
 /**
