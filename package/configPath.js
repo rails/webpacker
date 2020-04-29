54321cf8364e14dce4ddc5c9053a232e16064508
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2020-04-29 10:55:25 +0500

    Replace export with module exports

--- package/configPath.js
+++ package/configPath.js
@@ -1,3 +1,3 @@
 const { resolve } = require('path')
 
-export default process.env.WEBPACKER_CONFIG || resolve('config', 'webpacker.yml')
+module.exports = process.env.WEBPACKER_CONFIG || resolve('config', 'webpacker.yml')
