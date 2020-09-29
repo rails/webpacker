4e4e7795ade1a9c06bcc3acf08a8714dfd5443a3
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2020-09-29 12:24:02 +0100

    Remove extra installers

--- lib/install/examples/angular/hello_angular/index.ts
+++
@@ -1,8 +0,0 @@
-import './polyfills.ts';
-
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic().bootstrapModule(AppModule);
-});
