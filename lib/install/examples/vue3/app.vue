604aa29156db6c7bd92b5d3c139b77d088dfdc33
Vannsl <boehner.vanessa@gmail.com>
2020-10-12 13:31:50 +0200

    Add Vue3 installer (fix #2751)

---
+++ lib/install/examples/vue3/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>
+    {{ message }}
+  </p>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'HelloWorld',
+  setup() {
+    const message = ref('Hello World')
+
+    return {
+      message
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
