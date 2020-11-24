b2938bde3d3c17b2d48af6cc15bda113146b847d
Krists Ozols <krists.ozols@gmail.com>
2020-11-24 18:52:25 +0200

    Fix for Webpacker not detecting up changes in engines
    Fixes #2487

--- lib/webpacker/compiler.rb
+++ lib/webpacker/compiler.rb
@@ -53,10 +53,11 @@ class Webpacker::Compiler
 
     def watched_files_digest
       warn "Webpacker::Compiler.watched_paths has been deprecated. Set additional_paths in webpacker.yml instead." unless watched_paths.empty?
-
-      files = Dir[*default_watched_paths, *watched_paths].reject { |f| File.directory?(f) }
-      file_ids = files.sort.map { |f| "#{File.basename(f)}/#{Digest::SHA1.file(f).hexdigest}" }
-      Digest::SHA1.hexdigest(file_ids.join("/"))
+      Dir.chdir File.expand_path(config.root_path) do
+        files = Dir[*default_watched_paths, *watched_paths].reject { |f| File.directory?(f) }
+        file_ids = files.sort.map { |f| "#{File.basename(f)}/#{Digest::SHA1.file(f).hexdigest}" }
+        Digest::SHA1.hexdigest(file_ids.join("/"))
+      end
     end
 
     def record_compilation_digest
