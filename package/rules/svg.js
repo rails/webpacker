7d81bc40673f512aea77c31aa0aecf75b2189473
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2020-12-20 17:18:02 +0000

    Make mini data uri optional

--- package/rules/svg.js
+++ package/rules/svg.js
@@ -1,7 +1,23 @@
-const svgToMiniDataURI = require('mini-svg-data-uri')
+/* eslint global-require: 0 */
+/* eslint import/no-dynamic-require: 0 */
 
 module.exports = {
   test: /\.svg$/i,
   type: 'asset/inline',
-  generator: { dataUrl: (content) => svgToMiniDataURI(content.toString()) }
+  generator: {
+    dataUrl: (content) => {
+      let optimisedContent = content
+
+      try {
+        if (require.resolve('mini-svg-data-uri')) {
+          const svgToMiniDataURI = require('mini-svg-data-uri')
+          optimisedContent = svgToMiniDataURI(content.toString())
+        }
+      } catch (e) {
+        /* Work out what to print here */
+      }
+
+      return optimisedContent
+    }
+  }
 }
