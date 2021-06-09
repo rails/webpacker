a910d3f03adbd432108b382f616ae4bf75a75161
James Denness <james@cvpartner.no>
2021-06-09 12:08:10 +0200

    fix formatting

--- lib/webpacker/dev_server.rb
+++ lib/webpacker/dev_server.rb
@@ -1,5 +1,5 @@
 class Webpacker::DevServer
-  DEFAULT_ENV_PREFIX = 'WEBPACKER_DEV_SERVER'.freeze
+  DEFAULT_ENV_PREFIX = "WEBPACKER_DEV_SERVER".freeze
 
   # Configure dev server connection timeout (in seconds), default: 0.01
   # Webpacker.dev_server.connect_timeout = 1
@@ -16,11 +16,12 @@ class Webpacker::DevServer
       unless config.dev_server.disable_health_check
         Socket.tcp(host, port, connect_timeout: connect_timeout).close
       end
+
       true
     else
       false
     end
-  rescue StandardError
+  rescue
     false
   end
 
@@ -34,7 +35,7 @@ class Webpacker::DevServer
 
   def https?
     case fetch(:https)
-    when true, 'true', Hash
+    when true, "true", Hash
       true
     else
       false
@@ -42,7 +43,7 @@ class Webpacker::DevServer
   end
 
   def protocol
-    https? ? 'https' : 'http'
+    https? ? "https" : "http"
   end
 
   def host_with_port
@@ -58,13 +59,11 @@ class Webpacker::DevServer
   end
 
   private
+    def fetch(key)
+      ENV["#{env_prefix}_#{key.upcase}"] || config.dev_server.fetch(key, defaults[key])
+    end
 
-  def fetch(key)
-    ENV["#{env_prefix}_#{key.upcase}"] ||
-      config.dev_server.fetch(key, defaults[key])
-  end
-
-  def defaults
-    config.send(:defaults)[:dev_server] || {}
-  end
+    def defaults
+      config.send(:defaults)[:dev_server] || {}
+    end
 end
