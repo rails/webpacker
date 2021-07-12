67695588b33bfc35e545e8c03d1a3fcc53a30757
Guillaume Briday <guillaumebriday@gmail.com>
2021-07-12 12:05:56 +0200

    Raise an error when javascript_pack_tag or stylesheet_pack_tag are called more than once

--- lib/webpacker/helper.rb
+++ lib/webpacker/helper.rb
@@ -96,6 +96,9 @@ module Webpacker::Helper
   #   <%= javascript_pack_tag 'calendar' %>
   #   <%= javascript_pack_tag 'map' %>
   def javascript_pack_tag(*names, **options)
+    raise "To prevent duplicated chunks on the page, you should call javascript_pack_tag only once on the page." if defined?(@javascript_pack_tag_loaded)
+    @javascript_pack_tag_loaded = true
+
     javascript_include_tag(*sources_from_manifest_entrypoints(names, type: :javascript), **options)
   end
 
@@ -141,6 +144,9 @@ module Webpacker::Helper
   #   <%= stylesheet_pack_tag 'calendar' %>
   #   <%= stylesheet_pack_tag 'map' %>
   def stylesheet_pack_tag(*names, **options)
+    raise "To prevent duplicated chunks on the page, you should call stylesheet_pack_tag only once on the page." if defined?(@stylesheet_pack_tag_loaded)
+    @stylesheet_pack_tag_loaded = true
+
     return "" if Webpacker.inlining_css?
 
     stylesheet_link_tag(*sources_from_manifest_entrypoints(names, type: :stylesheet), **options)
