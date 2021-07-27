81dc763f3ae2b920aa6e84902b2bddd6c5475a3c
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-07-26 21:22:42 -0300

    Update helper_test.rb

--- test/helper_test.rb
+++ test/helper_test.rb
@@ -117,29 +117,41 @@ class HelperTest < ActionView::TestCase
   end
 
   def test_stylesheet_pack_tag_split_chunks
-    assert_equal \
-      %(<link rel="stylesheet" href="/packs/1-c20632e7baf2c81200d3.chunk.css" media="screen" />\n) +
+    assert_equal stylesheet_packs_with_chunks_tag("application", "hello_stimulus").in?([
+        %(<link rel="stylesheet" href="/packs/1-c20632e7baf2c81200d3.chunk.css" media="screen" />\n) +
         %(<link rel="stylesheet" href="/packs/application-k344a6d59eef8632c9d1.chunk.css" media="screen" />\n) +
         %(<link rel="stylesheet" href="/packs/hello_stimulus-k344a6d59eef8632c9d1.chunk.css" media="screen" />),
-      stylesheet_packs_with_chunks_tag("application", "hello_stimulus")
+      
+        %(<link rel="stylesheet" media="screen" href="/packs/1-c20632e7baf2c81200d3.chunk.css" />\n) +
+        %(<link rel="stylesheet" media="screen" href="/packs/application-k344a6d59eef8632c9d1.chunk.css" />\n) +
+        %(<link rel="stylesheet" media="screen" href="/packs/hello_stimulus-k344a6d59eef8632c9d1.chunk.css" />),
+      ]),
+      true
   end
 
   def test_stylesheet_pack_tag
-    assert_equal \
+    assert_equal stylesheet_pack_tag("bootstrap.css").in?([
       %(<link rel="stylesheet" href="/packs/bootstrap-c38deda30895059837cf.css" media="screen" />),
-      stylesheet_pack_tag("bootstrap.css")
+      
+      %(<link rel="stylesheet" media="screen" href="/packs/bootstrap-c38deda30895059837cf.css" />),
+    ]), true
   end
 
   def test_stylesheet_pack_tag_symbol
-    assert_equal \
+    assert_equal stylesheet_pack_tag(:bootstrap).in?([
       %(<link rel="stylesheet" href="/packs/bootstrap-c38deda30895059837cf.css" media="screen" />),
-      stylesheet_pack_tag(:bootstrap)
+      
+      %(<link rel="stylesheet" media="screen" href="/packs/bootstrap-c38deda30895059837cf.css" />),
+    ]), true
   end
 
   def test_stylesheet_pack_tag_splat
-    assert_equal \
+    assert_equal stylesheet_pack_tag("bootstrap.css", "application.css", media: "all").in?([
       %(<link rel="stylesheet" href="/packs/bootstrap-c38deda30895059837cf.css" media="all" />\n) +
         %(<link rel="stylesheet" href="/packs/application-dd6b1cd38bfa093df600.css" media="all" />),
-      stylesheet_pack_tag("bootstrap.css", "application.css", media: "all")
+      
+      %(<link rel="stylesheet" media="all" href="/packs/bootstrap-c38deda30895059837cf.css" />\n) +
+        %(<link rel="stylesheet" media="all" href="/packs/application-dd6b1cd38bfa093df600.css" />),
+    ]), true
   end
 end
