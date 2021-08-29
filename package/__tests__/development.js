ca089e5676989f1e698be02aa86be7797f47bb4a
Guillaume Briday <guillaumebriday@gmail.com>
2021-08-29 10:32:24 +0200

    Fixing specs and lints

--- package/__tests__/development.js
+++ package/__tests__/development.js
@@ -14,7 +14,7 @@ describe('Development environment', () => {
     test('should use development config and environment including devServer if WEBPACK_SERVE', () => {
       process.env.RAILS_ENV = 'development'
       process.env.NODE_ENV = 'development'
-      process.env.WEBPACK_DEV_SERVER = 'true'
+      process.env.WEBPACK_SERVE = 'true'
       const { webpackConfig } = require('../index')
 
       expect(webpackConfig.output.path).toEqual(resolve('public', 'packs'))
@@ -31,7 +31,7 @@ describe('Development environment', () => {
     test('should use development config and environment if WEBPACK_SERVE', () => {
       process.env.RAILS_ENV = 'development'
       process.env.NODE_ENV = 'development'
-      process.env.WEBPACK_DEV_SERVER = undefined
+      process.env.WEBPACK_SERVE = undefined
       const { webpackConfig } = require('../index')
 
       expect(webpackConfig.output.path).toEqual(resolve('public', 'packs'))
